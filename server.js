70f1e3c974d70b16f8d07a1543533fd14ce6d7f4
junting <10355098@st.thsh.tp.edu.tw>
2023-04-24 23:11:40 +0800

    try to fix bug

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port =  3000;
+const port =  process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
-server.listen(PORT, () => {
-    console.log(`JSON Server is running on port ${PORT}`);
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`);
 });
